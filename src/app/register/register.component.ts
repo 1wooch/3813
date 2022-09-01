2bbe9eb4921fd326b504e7af8458401e6f821a17
Wonwoo Choi <onechoo1106@gmail.com>
2022-09-02 00:06:18 +1000

    register page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
 
-  constructor() { }
+
+
+export class RegisterComponent implements OnInit {
+  form:any={
+    username:null,
+    email:null,
+    password:null,
+    age:null,
+    birthdate:null
+  };
+  isSuccessful =false;
+  isSignUpFailed=false;
+  errorMessage='';
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit():void{
+    const{username,birthdate,age,email,password}=this.form;
+    this.authService.register(username,email,password,age,birthdate).subscribe({
+      next:data=>{
+        this.isSuccessful =true;
+        this.isSignUpFailed=false;
+      },
+      error:err=>{
+        this.errorMessage=err.error.message;
+        this.isSignUpFailed=true;
+
+      }
+    });
   }
 
 }
